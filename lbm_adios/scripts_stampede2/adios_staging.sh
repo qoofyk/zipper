03af9229fbee39c5761f0728ac918449890ad608
Feng Li <lifen@iupui.edu>
2017-10-04 08:52:36 -0500

    before merge new files

--- lbm_adios/scripts_stampede2/adios_staging.sh
+++ lbm_adios/scripts_stampede2/adios_staging.sh
@@ -1,15 +1,15 @@
-####################################################
-# common commands for all experiments
+#################################################### 
+# common commands for all experiments 
+
 BUILD_DIR=${PBS_O_WORKDIR}/build
 
 BIN_PRODUCER=${BUILD_DIR}/bin/run_lbm;
-BIN_CONSUMER=${BUILD_DIR}/bin/native_staging_read;
+BIN_CONSUMER=${BUILD_DIR}/bin/adios_staging_read;
 
 #This job runs with 3 nodes  
 #ibrun in verbose mode will give binding detail  #BUILD=${PBS_O_WORKDIR}/build_dspaces/bin
 DS_SERVER=${PBS_O_HOME}/envs/Dataspacesroot/bin/dataspaces_server
 PBS_RESULTDIR=${SCRATCH_DIR}/results
-
 
 DS_CLIENT_PROCS=$((${PROCS_PRODUCER} + ${PROCS_CONSUMER}))
 
@@ -17,24 +17,12 @@ echo "${DS_CLIENT_PROCS} clients, $PROCS_SERVER server"
 
 mkdir -pv ${PBS_RESULTDIR}
 mkdir -pv ${SCRATCH_DIR}
-
-## traces
-SERVER_TRACE_DIR=${PBS_RESULTDIR}/server_trace
-PRODUCER_TRACE_DIR=${PBS_RESULTDIR}/producer_trace
-CONSUMER_TRACE_DIR=${PBS_RESULTDIR}/consumer_trace
-mkdir -pv ${SERVER_TRACE_DIR}
-mkdir -pv ${PRODUCER_TRACE_DIR}
-mkdir -pv ${CONSUMER_TRACE_DIR}
-
-
 cd ${SCRATCH_DIR}
 cp -R ${PBS_O_WORKDIR}/adios_xmls ${SCRATCH_DIR}
 
-
-
-## Clean up
-rm -f conf *.log srv.lck
-rm -f dataspaces.conf
+## this is a clean working dir
+#rm -f conf *.log srv.lck
+#rm -f dataspaces.conf
 
 ## Create dataspaces configuration file
 # note that we now have 400 regions
@@ -42,22 +30,24 @@ rm -f dataspaces.conf
 #dims = 5, 300000, 10
 #dims = 2, 1500000, 1
 # 64*64*256 will generate 1048576 lines
+echo "total number of lines is being calculated"
 DS_LIMIT=$((${FILESIZE2PRODUCE}*${FILESIZE2PRODUCE}*${FILESIZE2PRODUCE}*${PROCS_PRODUCER}/16)) # make sure dspaces can hold all data
 
 echo "total number of lines is $DS_LIMIT"
 
 echo "## Config file for DataSpaces
 ndim = 2
-dims = 2, $((DS_LIMIT)) 
-max_versions = 20
+dims = 2, $((DS_LIMIT))
+max_versions = 5
 max_readers = 1
-lock_type = 2
+# lock_type = 2
 " > dataspaces.conf
 echo "DS_LIMIT= $DS_LIMIT"
   
 
 #LAUNCHER="ibrun -v"
 LAUNCHER="ibrun"
+echo "use transport method $CMTransport with CMTransportVerbose=$CMTransportVerbose"
 
 ## Run DataSpaces servers
 CMD_SERVER="$LAUNCHER -n $PROCS_SERVER -o 0 ${DS_SERVER} -s $PROCS_SERVER -c $DS_CLIENT_PROCS"
@@ -73,10 +63,12 @@ CMD_PRODUCER="$LAUNCHER -n $PROCS_PRODUCER -o $((${SLURM_NTASKS_PER_NODE}*${num_
 $CMD_PRODUCER  &> ${PBS_RESULTDIR}/producer.log &
 echo "producer applciation lauched: $CMD_PRODUCER"
 
-CMD_CONSUMER="$LAUNCHER -n $PROCS_CONSUMER -o $((${SLURM_NTASKS_PER_NODE}*${num_nodes_server}+${PROCS_PRODUCER})) ${BIN_CONSUMER} ${NSTOP} ${FILESIZE2PRODUCE} ${PROCS_PRODUCER}"
+CMD_CONSUMER="$LAUNCHER -n $PROCS_CONSUMER -o $((${SLURM_NTASKS_PER_NODE}*${num_nodes_server}+${PROCS_PRODUCER})) ${BIN_CONSUMER}"
 $CMD_CONSUMER  &> ${PBS_RESULTDIR}/consumer.log &
 echo " consumer applciation lauched $CMD_CONSUMER"
 
 ## Wait for the entire workflow to finish
 wait
+
+
 
