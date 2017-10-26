f220e9c0b73c6db20e0dd334ebc0d198999b53b7
Feng Li <lifen@iupui.edu>
2017-10-26 10:35:05 -0500

    reorganize in stampede2

--- lbm_adios/transports.h
+++ lbm_adios/transports.h
@@ -1,3 +1,5 @@
+#ifndef TRANSPORT_H
+#define TRANSPORT_H
 /*
  * @brief type definition of different tranports
  *
@@ -64,5 +66,73 @@ typedef uint8_t transport_method_t;
  *
  * @return  current tranport method
  */
-transport_method_t get_current_transport();
+//#include "utility.h"
 
+static transport_method_t get_current_transport(){
+    uint8_t major, minor; // method id
+    char * transport_string; //input from user
+
+    /*
+     * must define tranport method!
+     */
+    minor = MAJOR_NO_DEF ;
+    minor = MINOR_NO_DEF;
+
+    /*
+     * get the configuration from environment variable
+     */
+    if((transport_string = getenv("MyTransport")) != NULL){
+        //clog_debug(CLOG(MY_LOGGER),"get string %s\n", transport_string);
+        printf("get string %s\n", transport_string);
+
+        /*
+         * adios-mpiio
+         */ 
+        if(strcmp(transport_string, "ADIOS_DISK_MPIIO") == 0){
+            major = ADIOS_DISK;
+            minor = MPIIO;
+        }
+
+        /*
+         * adios staging
+         */
+        else if (strcmp(transport_string, "ADIOS_STAGING_DSPACES") == 0){
+            major = ADIOS_STAGING;
+            minor = DSPACES;
+        }
+        else if (strcmp(transport_string, "ADIOS_STAGING_DIMES") == 0){
+            major = ADIOS_STAGING;
+            minor = DIMES;
+        }
+
+        else if (strcmp(transport_string, "ADIOS_STAGING_FLEXPATH") == 0){
+            major = ADIOS_STAGING;
+            minor = FLEXPATH;
+        }
+
+        /*
+         * native staging
+         */
+
+        else if (strcmp(transport_string, "NATIVE_STAGING_DSPACES") == 0){
+            major = NATIVE_STAGING;
+            minor = DSPACES;
+        }
+        else if (strcmp(transport_string, "NATIVE_STAGING_DIMES") == 0){
+            major = NATIVE_STAGING;
+            minor = DIMES;
+        }
+
+        else{
+            minor = MAJOR_NO_DEF ;
+            minor = MINOR_NO_DEF;
+        }
+    }
+
+
+    transport_method_t transport = construct_method(major, minor);
+    printf("tranport code %x\n", transport);
+    return transport;
+}
+
+#endif
