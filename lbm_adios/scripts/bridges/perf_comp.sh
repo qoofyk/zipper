c47a07c4d867fd22dfe5bd2f1b49323b650665f3
lifen.bridges <lifen@iupui.edu>
2017-12-01 09:54:33 -0500

    queuing larger experiments

--- lbm_adios/scripts/bridges/perf_comp.sh
+++ lbm_adios/scripts/bridges/perf_comp.sh
@@ -1,19 +1,20 @@
 #! /bin/bash
 
 # first job - no dependencies
-jid1=$(sbatch scripts/bridges/512v256/native_dspaces_nokeep.job|awk '{print $NF}')
+CaseName="bridges/256v128"
+jid1=$(sbatch scripts/${CaseName}/native_dspaces_nokeep.job|awk '{print $NF}')
 
-jid2=$(sbatch --dependency=afterany:$jid1 scripts/bridges/512v256/native_dimes_nokeep.job|awk '{print $NF}')
+jid2=$(sbatch --dependency=afterany:$jid1 scripts/${CaseName}/native_dimes_nokeep.job|awk '{print $NF}')
 
-jid3=$(sbatch --dependency=afterany:$jid2 scripts/bridges/512v256/adios_dspaces_nokeep.job|awk '{print $NF}')
+jid3=$(sbatch --dependency=afterany:$jid2 scripts/${CaseName}/adios_dspaces_nokeep.job|awk '{print $NF}')
 
-jid4=$(sbatch --dependency=afterany:$jid3 scripts/bridges/512v256/adios_dimes_nokeep.job|awk '{print $NF}')
+jid4=$(sbatch --dependency=afterany:$jid3 scripts/${CaseName}/adios_dimes_nokeep.job|awk '{print $NF}')
 
-jid5=$(sbatch --dependency=afterany:$jid4 scripts/bridges/512v256/mpiio.job|awk '{print $NF}')
+jid5=$(sbatch --dependency=afterany:$jid4 scripts/${CaseName}/mpiio.job|awk '{print $NF}')
 
-jid6=$(sbatch --dependency=afterany:$jid5 scripts/bridges/512v256/adios_flexpath.job|awk '{print $NF}')
+jid6=$(sbatch --dependency=afterany:$jid5 scripts/${CaseName}/adios_flexpath.job|awk '{print $NF}')
 
-jid7=$(sbatch --dependency=afterany:$jid6 scripts/bridges/512v256/sim_only.job|awk '{print $NF}')
+jid7=$(sbatch --dependency=afterany:$jid6 scripts/${CaseName}/sim_only.job|awk '{print $NF}')
 
 # show dependencies in squeue output:
 squeue -u $USER -o "%.8A %.4C %.10m %.20E"
