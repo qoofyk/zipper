5b2b56b95d1f4b35fcd56def42525212d04e50b0
Feng Li <lifen@iupui.edu>
2018-01-15 09:37:04 -0600

    add itac

--- lbm_adios/cmake/common.cmake
+++ lbm_adios/cmake/common.cmake
@@ -15,6 +15,20 @@ elseif(CMAKE_BUILD_TYPE STREQUAL Stampede)
   set(LAMMPS_LIBRARY_STATIC "/work/04446/tg837458/stampede2/software/lammps-16Feb16-lib/src/static_libs_feng/liblammps.a" CACHE PATH "lammps library path")
   set(DECAF_PREFIX "$ENV{WORK}/software/install" CACHE PATH "decaf path")
 
+elseif(CMAKE_BUILD_TYPE STREQUAL Stampede_ITAC)
+  message("-- ${CMAKE_CURRENT_SOURCE_DIR} > Stampede itac build")
+  set(CMAKE_C_COMPILER  icc)
+  set(CMAKE_CXX_COMPILER  icpc)
+  set(TRANSPORT_LIB "$ENV{WORK}/envs/transports_icc_impi" CACHE PATH "The path to transport libs.")
+  set(CMAKE_C_FLAGS "-tcollect=VT -trace -O3 -Wall -Wextra -xCORE-AVX2 -axMIC-AVX512 -fPIC" CACHE STRING "cflags")
+  set(CMAKE_CXX_FLAGS "-tcollect=VT -trace -O3 -Wall -Wextra -xCORE-AVX2 -axMIC-AVX512 -fPIC" CACHE STRING "cxxflags")
+  set(LAMMPS_INCLUDE_DIR "/work/04446/tg837458/stampede2/software/lammps-16Feb16/src" CACHE PATH "lammps include path")
+  set(LAMMPS_LIBRARY "/work/04446/tg837458/stampede2/software/lammps-16Feb16-lib/src/liblammps.so" CACHE PATH "lammps library path")
+  set(LAMMPS_LIBRARY_STATIC "/work/04446/tg837458/stampede2/software/lammps-16Feb16-lib/src/static_libs_feng/liblammps.a" CACHE PATH "lammps library path")
+  set(DECAF_PREFIX "$ENV{WORK}/software/install" CACHE PATH "decaf path")
+
+
+
 elseif(CMAKE_BUILD_TYPE STREQUAL Deb_Stam)
   message("-- ${CMAKE_CURRENT_SOURCE_DIR} > Stampede debug  build")
   set(CMAKE_C_COMPILER  icc)
@@ -59,12 +73,12 @@ elseif(CMAKE_BUILD_TYPE STREQUAL Bridges_TAU)
   set(BOOST_ROOT "$ENV{WORK}/software/install" CACHE PATH "Boost path")
   set(DECAF_PREFIX "$ENV{WORK}/software/install_tau" CACHE PATH "decaf path")
 
-else()
-    message(FATAL_ERROR "-- need build type")
-  
+ 
 endif()
 
 message("-- Including transport method in ${TRANSPORT_LIB} is loaded...")
+message("-- build type not set")
+ 
 
 #set(CMAKE_CXX_STANDARD 14)
 #set(GCC_COVERAGE_COMPILE_FLAGS "-fPIC -msse3")
---
+++ lbm_adios/docs/itac.md
@@ -0,0 +1,7 @@
+
+1. automatically instrumentation
+    -tcollect -trace
+    not successful with cmakea:
+        VT_fileio.c:(.text+0x4d5): undefined reference to `PMPI_Comm_rank'
+2. manually instrumentation
+    
