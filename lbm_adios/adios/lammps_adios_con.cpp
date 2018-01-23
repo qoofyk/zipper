ebd7421bc609b298210fd188a3365ee751f111a1
Feng Li <lifen@iupui.edu>
2018-01-22 19:51:01 -0600

    tmp

--- lbm_adios/adios/lammps_adios_con.cpp
+++ lbm_adios/adios/lammps_adios_con.cpp
@@ -43,6 +43,40 @@ int class_id;
 int analysis_id;
 #endif
 
+/*
+ * slice in dimension y
+ */
+void
+slice(uint64_t length, uint64_t *s, uint64_t *e, int rank, int mpisize)
+{
+    uint64_t start = 0;
+    uint64_t end = 0;
+    uint64_t rem = length % mpisize;
+
+    start = length/mpisize * rank;
+    end = length/mpisize * (rank+1);
+    *s = start;
+    *e = end;
+    
+    /* If our MPI size is greater
+       than the number of y dimensions,
+       then read the whole thing. */
+    if (mpisize > length) {
+	*e = length;
+	*s = 0;
+	return;\
+    }
+    if (end > length) {
+        end = length;
+        *e = end;
+        return;
+    }
+    if (rank == mpisize-1) {
+        end += rem;
+        *e = end;
+    }
+}
+
 //#define DEBUG_Feng
 int main (int argc, char ** argv){
 
@@ -135,30 +169,128 @@ int main (int argc, char ** argv){
     }
 
 
+    /* prepare adios
+     */
+    ADIOS_SELECTION *global_range_select;
+    int         NX, NY, NZ; 
+    double      *t;
+
+
     char filename[256];
 
     // append file name
     sprintf(filename, "atom.bp");
-    ADIOS_FILE * f = adios_read_open (filename, method, comm, ADIOS_LOCKMODE_CURRENT, -1);
+    ADIOS_FILE * afile = adios_read_open (filename, method, comm, ADIOS_LOCKMODE_CURRENT, -1);
     //ADIOS_FILE * f = adios_read_open (filename, method, comm, ADIOS_LOCKMODE_NONE, 0);
 
-     if (f == NULL)
+     if (afile == NULL)
     {
         printf ("rank %d, %s\n",rank, adios_errmsg());
         return -1;
     }
 
-    ADIOS_VARINFO * v = adios_inq_var (f, "array");
+    int ii = 0;
+    while(adios_errno != err_end_of_stream){       
+        ADIOS_VARINFO *nx_info = adios_inq_var(afile, "/scalar/dim/NX");
+        ADIOS_VARINFO *ny_info = adios_inq_var(afile, "/scalar/dim/NY");
+	ADIOS_VARINFO *nz_info = adios_inq_var(afile, "/scalar/dim/NZ");
+
+	ADIOS_VARINFO *arry = adios_inq_var( afile, "var_2d_array");
+    //ADIOS_VARINFO * v = adios_inq_var (f, "array");
     /* Using less readers to read the global array back, i.e., non-uniform */
+
+	uint64_t my_ystart, my_yend;
+	slice(arry->dims[1], &my_ystart, &my_yend, rank, nprocs);
+
+	/* printf("rank: %d my_ystart: %d, my_yend: %d\n", */
+	/*        rank, (int)my_ystart, (int)my_yend); */
+
+	uint64_t xcount = arry->dims[0];
+	uint64_t ycount = my_yend - my_ystart;
+	uint64_t zcount = arry->dims[2];
+
+	uint64_t starts[] = {0, my_ystart, 0};
+	uint64_t counts[] = {xcount, ycount, zcount};
+
+	/* printf("rank: %d starts: %d %d %d. counts: %d %d %d\n", */
+	/*        rank, */
+	/*        (int)starts[0], (int)starts[1], (int)starts[2], */
+	/*        (int)counts[0], (int)counts[1], (int)counts[2]); */
+
+	global_range_select = adios_selection_boundingbox(arry->ndim, starts, counts);
+
+	int nelem = xcount*ycount*zcount;
+
+        if(nx_info->value) {
+            NX = *((int *)nx_info->value);
+        }
+        if(ny_info->value){
+            NY= *((int*)ny_info->value);
+        }
+        if(nz_info->value){
+            NZ= *((int*)nz_info->value);
+        }
+    
+	if(rank == 0){
+	    int n;
+	    printf("[%.2f]:adios global dim(column based)step %d, dims: [ ",MPI_Wtime(), ii);
+	    for(n=0; n<arry->ndim; n++){
+		printf("%d ", (int)arry->dims[n]);
+	    }
+	    printf("]\n");
+	}
+    
+        /* Allocate space for the arrays */
+        if(!t){
+            int arr_size = sizeof(double) * nelem;
+            t = (double *) malloc (arr_size);
+            memset(t, 0, arr_size);
+        }
+        //fprintf(stderr, "t %p\n", t);
+      
+        /* Read the arrays */        
+        adios_schedule_read (afile, 
+                             global_range_select, 
+                             "var_2d_array", 
+                             0, 1, t);
+   /* adios_schedule_read (afile,*/
+				 /*&scalar_block_select,*/
+				 /*"test_scalar",*/
+				 /*0, 1, &test_scalar);*/
+
+        adios_perform_reads (afile, 1);                
+    
+        //sleep(20);
+    
+#ifdef DEBUG
+        printf("Rank=%d: test_scalar: %d step: %d, t[0,5+x] = [", rank, test_scalar, ii);
+        for(j=0; j<nelem; j++) {
+            printf(", %6.2f", t[j]);
+        }
+        printf("]\n\n");
+#endif
+        adios_release_step(afile);
+        adios_advance_step(afile, 0, 30);
+        ii++;
+        //MPI_Barrier (comm);
+        //sleep(1);
+    }
+    //
+    adios_read_close(afile);
+
+    if(t){
+        free(t);
+    }
 
 
     /* 
      * get the dimension info
      */
+#if 0
     uint64_t slice_size = v->dims[1]/nprocs;
     start[1] = slice_size * rank;
     if (rank == nprocs-1) /* last rank may read more lines */
-        slice_size = slice_size + v->dims[2]%nprocs;
+        slice_size = slice_size + v->dims[1]%nprocs;
     count[1] = slice_size;
 
 
@@ -248,6 +380,7 @@ int main (int argc, char ** argv){
     adios_free_varinfo (v);
     adios_read_close (f);
     MPI_Barrier (comm);
+#endif
     adios_read_finalize_method (method);
 
     MPI_Finalize ();
