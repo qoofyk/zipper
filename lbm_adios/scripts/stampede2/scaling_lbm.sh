1587d43358b043b82d9f4de186eb74f69de735b5
Feng Li <lifen@iupui.edu>
2018-04-09 09:54:48 -0500

    missing files

--- lbm_adios/scripts/stampede2/scaling_lbm.sh
+++ lbm_adios/scripts/stampede2/scaling_lbm.sh
@@ -13,31 +13,20 @@ fi
 #USE_RES="--reservation=fli5"
 unset USE_RES # do not use reservation
 # first job - no dependencies
-CaseName="bridges/256v128"
+CaseName="scripts/stampede2/scaling_exp/lbm_mpiio"
 
 if [ x"$GEN_TRACE" == "x" ]; then
     echo "no trace 100 steps!"
-    jid1=$(${launch_cmd} ${USE_RES} ${PRE_RUN} scripts/${CaseName}/native_dspaces_nokeep.job|awk '{print $NF}')
+    jid1=$(${launch_cmd} ${USE_RES}  ${PRE_RUN} ${CaseName}/68vs34.job|awk '{print $NF}')
 
-    jid2=$(${launch_cmd} ${USE_RES} --dependency=afterany:$jid1 scripts/${CaseName}/native_dimes_nokeep.job|awk '{print $NF}')
+    jid2=$(${launch_cmd} ${USE_RES} --dependency=afterany:$jid1 ${CaseName}/136vs68.job|awk '{print $NF}')
+   
 
-    jid3=$(${launch_cmd} ${USE_RES} --dependency=afterany:$jid2 scripts/${CaseName}/adios_dspaces_nokeep.job|awk '{print $NF}')
+    jid3=$(${launch_cmd} ${USE_RES} --dependency=afterany:$jid2 ${CaseName}/272vs136.job|awk '{print $NF}')
 
-    jid4=$(${launch_cmd} ${USE_RES} --dependency=afterany:$jid3 scripts/${CaseName}/adios_dimes_nokeep.job|awk '{print $NF}')
+    jid4=$(${launch_cmd} ${USE_RES} --dependency=afterany:$jid3 ${CaseName}/544vs272.job|awk '{print $NF}')
 
-    jid5=$(${launch_cmd} ${USE_RES} --dependency=afterany:$jid4 scripts/${CaseName}/mpiio.job|awk '{print $NF}')
-
-    jid6=$(${launch_cmd} ${USE_RES} --dependency=afterany:$jid5 scripts/${CaseName}/adios_flexpath.job|awk '{print $NF}')
-
-    jid7=$(${launch_cmd} ${USE_RES} --dependency=afterany:$jid6 scripts/${CaseName}/decaf.job|awk '{print $NF}')
-
-    jid8=$(${launch_cmd} ${USE_RES} --dependency=afterany:$jid7 scripts/${CaseName}/sim_only.job|awk '{print $NF}')
-else
-    echo "with trace!"
-    jid1=$(${launch_cmd} ${USE_RES} ${PRE_RUN} scripts/${CaseName}/native_dspaces_nokeep.job|awk '{print $NF}')
-
-    jid2=$(${launch_cmd} ${USE_RES} --dependency=afterany:$jid1 scripts/${CaseName}/native_dimes_nokeep.job|awk '{print $NF}')
-    jid3=$(${launch_cmd} ${USE_RES} --dependency=afterany:$jid2 scripts/${CaseName}/mpiio.job|awk '{print $NF}')
+    jid5=$(${launch_cmd} ${USE_RES} --dependency=afterany:$jid4 ${CaseName}/1088vs544.job|awk '{print $NF}')
 fi
 
 
