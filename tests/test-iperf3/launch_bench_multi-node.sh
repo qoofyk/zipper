addfc2a00838a50069d98d11391e618e3f83c7a9
Feng Li <lifen@iupui.edu>
2020-11-03 12:14:30 -0600

    bench with redis-benchmark

--- tests/test-iperf3/launch_bench_multi-node.sh
+++ tests/test-iperf3/launch_bench_multi-node.sh
@@ -24,11 +24,11 @@
 #----------------------------------------------------
 
 #SBATCH -J bench-4x1          # Job name
-#SBATCH -o results/bench-4x1.o%j       # Name of stdout output file
+#SBATCH -o results/bench.o%j       # Name of stdout output file
 #SBATCH -p normal          # Queue (partition) name
-#SBATCH -N 2               # Total # of nodes 
+#SBATCH -N 8               # Total # of nodes 
 #SBATCH --tasks-per-node=1
-#SBATCH -t 00:05:00        # Run time (hh:mm:ss)
+#SBATCH -t 00:10:00        # Run time (hh:mm:ss)
 
 # Other commands must follow all #SBATCH directives...
 
@@ -37,7 +37,7 @@ pwd
 date
 
 # Launch MPI code...
-all_ips=(129.114.104.67 129.114.16.85 129.114.16.50 129.114.16.79 129.114.16.97 129.114.16.77 129.114.16.60 129.114.16.45)
+all_ips=(129.114.16.101 129.114.16.63 129.114.16.67 129.114.16.76 129.114.16.98 129.114.16.45 129.114.16.50 129.114.16.60)
 
 echo "test cloud endpoints: ${all_ips[*]}"
 
@@ -47,25 +47,48 @@ mkdir $RESULT_FOLDER
 nodes=(`scontrol show hostname $SLURM_NODELIST`)
 echo "all nodes: $nodes"
 
-for nr_conn in 1
+MY_PASS=5ba4239a1a2b7cd8131da1e557f4264df7ef2083f8895eab1d30384f870a9d87
+REDIS_BENCH=$WORK/zipper-runtime/extern/redis/src/redis-benchmark
+#NR_ELEMS_PER_CONN=1000
+NR_ELEMS_PER_CONN=500 # shorttest
+elem_size=90000  #10000 elems each have 9bytes
+
+echo "Total of $elapsed seconds elapsed for process"
+
+
+for nr_conn in 1 4 16 64
 do
   echo "================================================="
   echo "==========Test $nr_conn connections: ============"
   echo "================================================="
+
+  start_time=`date +%s%N`
+
   for nodeid in $(seq 0 $((SLURM_NNODES-1))); do
     nodename=${nodes[${nodeid}]}
     echo "placing on node ${nodename}"
     # check process host binding
     # ibrun -n 1 -o ${nodeid} hostname &> $RESULT_FOLDER/node${nodeid} &
-    outfile=$RESULT_FOLDER/log.node${nodeid}
+    outfile=$RESULT_FOLDER/log.cpn${nr_conn}.node${nodeid}
     date> $outfile
-    mpirun -n 1 -hosts ${nodename} iperf3 -c ${all_ips[nodeid]} -t 30 -l 32K -p 31993 -P $nr_conn &>> $outfile &     # Use ibrun instead of mpirun or mpiexec
+    # mpirun -n 1 -hosts ${nodename} iperf3 -c ${all_ips[nodeid]} -t 30 -l 32K -p 31993 -P $nr_conn &>> $outfile &     # Use ibrun instead of mpirun or mpiexec
+
+    nr_elems=$((NR_ELEMS_PER_CONN*nr_conn))
+    mpirun -n 1 -hosts ${nodename} $REDIS_BENCH -h ${all_ips[nodeid]} -p 30379 -a $MY_PASS -r 10000 -n $nr_elems -t set -d ${elem_size} -c ${nr_conn} -P 8  &>> $outfile &
   done
   #mpirun -n 1 iperf3 -c ${all_ips[1]} -p 1993 -P $nr_conn &     # Use ibrun instead of mpirun or mpiexec
   #mpirun -n 1 iperf3 -c ${all_ips[2]} -p 1993 -P $nr_conn &     # Use ibrun instead of mpirun or mpiexec
   #mpirun -n 1 iperf3 -c ${all_ips[3]} -p 1993 -P $nr_conn &     # Use ibrun instead of mpirun or mpiexec
+  wait
+  end_time=`date +%s%N`
+  elapsed=`python3 -c "print(($end_time-$start_time)/1000000000)"`
+
+  data_total_size=`python3 -c "print($nr_elems*$elem_size*$SLURM_NNODES/1000000.0)"`
+  throughput=`python3 -c "print($data_total_size/$elapsed)"`
+
+  echo -e "STATS: nr_conn_per_node\t MB objects\t seconds\t MB/s"
+  echo -e "STATS: $nr_conn\t $data_total_size\t $elapsed \t $throughput"
+  sleep 10
 done
 
-wait
-echo "All complete"
 #cp results/bench-4x1.o${PBS_JOB_ID} $RESULT_FOLDER
