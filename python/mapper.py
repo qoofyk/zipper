4265ef8d746c0094faf0c11849c15133584880c7
Feng Li <fengggli@yahoo.com>
2020-11-13 20:12:24 +0000

    0.2.9

--- python/mapper.py
+++ python/mapper.py
@@ -1,14 +1,14 @@
 import sys
 import math
-def continuous_mapper(global_id, global_size, num_groups):
-    group_size = math.ceil(global_size/num_groups) # round up
-    local_id = global_id % group_size
-    group_id = global_id // group_size
+def contiguous_mapper(mpi_rank, mpi_size, num_endpoints):
+    group_size = math.ceil(mpi_size/num_endpoints) # round up
+    local_id = mpi_rank % group_size
+    group_id = mpi_rank // group_size
     return (group_id, local_id)
 
 def generate_endpoint_file(endpoint_list, mpi_size, mapper_func):
     for i in range(mpi_size):
-        group_id, local_id = continuous_mapper(i, mpi_size, len(endpoint_list))
+        group_id, local_id = contiguous_mapper(i, mpi_size, len(endpoint_list))
         print(endpoint_list[group_id], local_id)
 
 
@@ -19,4 +19,4 @@ if __name__ == "__main__":
     mpi_size = int(sys.argv[1])
     endpoint_list = sys.argv[2:]
     #print("The endpoints are: " , endpoint_list)
-    generate_endpoint_file(endpoint_list, mpi_size, continuous_mapper)
+    generate_endpoint_file(endpoint_list, mpi_size, contiguous_mapper)
